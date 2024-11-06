316da3bf3a9032fb88e35eb0b5d05b06c65e7244
Yashas <yashasjob@gmail.com>
2024-11-06 13:29:27 +0530

    c2

--- src/components/CardWishlist.js
+++ src/components/CardWishlist.js
@@ -1,21 +1,21 @@
 import React from "react";
 
-const CardProduct = ({ product, addToCart }) => {
+const CardWishlist = ({ wishlist, addToCart }) => {
   return (
     <div className="custom-card card">
       <img
-        src={product.product_image}
-        className="card-img-top"
+        src={wishlist.product_image}
+        class="card-img-top"
         style={{ maxHeight: "200px" }}
         alt="..."
       />
-      <div className="card-body">
-        <h5 className="card-title">{product.product_name}</h5>
-        <p className="card-text">{product.product_desc}</p>
-        <strong className="text-muted mt-2">Rs {product.product_price}</strong>
+      <div class="card-body">
+        <h5 class="card-title">{wishlist.product_name}</h5>
+        <p class="card-text">{wishlist.product_desc}</p>
+        <strong className="text-muted mt-2">Rs {wishlist.product_price}</strong>
         <button
-          onClick={() => addToCart(product)}
-          className="btn btn-primary btn-block mt-3"
+          onClick={() => addToCart(wishlist)}
+          class="btn btn-primary btn-block mt-3"
         >
           Add To Cart
         </button>
@@ -24,4 +24,4 @@ const CardProduct = ({ product, addToCart }) => {
   );
 };
 
-export default CardProduct;
+export default CardWishlist;
